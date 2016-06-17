f47f09ccb991a7907818d38e16c83dfec093b5ec
Taha Pourjalali <taha.hojati@gmail.com>
2016-06-17 11:34:06 -1000

    Im middle of creating project view template

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('taha', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('portfolio', {
-                url: '/',
+                url: '/portfolio',
                 views: {
                     content: {
                         templateUrl: 'views/portfolio.html',
@@ -10,7 +10,7 @@ angular.module('taha', ['ui.router'])
                 }
             })
             .state('bio', {
-                url: 'bio',
+                url: '/bio',
                 views: {
                     content: {
                         templateUrl: 'views/bio.html',
@@ -19,7 +19,7 @@ angular.module('taha', ['ui.router'])
                 }
             })
             .state('contact', {
-                url: 'contact',
+                url: '/contact',
                 views: {
                     content: {
                         templateUrl: 'views/contact.html',
@@ -28,7 +28,7 @@ angular.module('taha', ['ui.router'])
                 }
             })
             .state('project', {
-                url: 'project/:id',
+                url: '/project/:id',
                 views: {
                     content: {
                         templateUrl: 'views/project.html',
@@ -37,7 +37,7 @@ angular.module('taha', ['ui.router'])
                 }
             });
 
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/portfolio');
 
     })
     .filter('evensFilter', function() {
@@ -50,12 +50,39 @@ angular.module('taha', ['ui.router'])
         };
     })
 
+.controller('PortfolioController', ['$scope', 'projectFactory', function($scope ,projectFactory) {
+
+    $scope.projects = projectFactory;
+}])
+
+.controller('ProjectController', ['$scope', 'projectFactory', '$stateParams', 'frameworkProvider', function($scope, projectFactory, $stateParams, frameworkProvider){
+    $scope.project = projectFactory[parseInt($stateParams.id,10)];
+    $scope.frameworks = [];
+    for(var i in $scope.projects){
+        $scope.frameworks.push(frameworkProvider['i']);
+    }
+}])
+
+.factory('frameworkProvider',[function(){
+    return {
+        'angular': {
+            icon:'sddfd'
+        }
+    };
+}])
+
 .factory('projectFactory',[function(){
     var fac = [{
             id: 1,
             title: 'Frogger',
-            description: "Lorem ipsum dolor sit amet, paulo admodum an sit, denique noluisse vim ne. Vix cu vocent regione insolens. Sed te purto debitis molestiae. Ut quod dico alienum nam, odio iudico oportere eu vis. Sea aliquam maluisset hendrerit et, sea eu expetendis efficiendi<br/> nstructior, semper dictas mei ad. Aliquid appellantur qui ea, eum posse invidunt necessitatibus at, accusam apeirian per in. Vel et velit interpretaris. Sit ut porro melius habemus, consetetur dissentias vel cu. Iisque<br/>  integre lucilius pro no,an dico veritus est. Brute recusabo mea ei, quo sint constituam deterruisset te, velit ubique ei mea. Cum eu inani copiosae aliquando.Cum an vide graece utamur, his et dico equidem vulputate. Exercinoster ea vix. Ad amet dicunt vix, modus oblique in duo. Sonet docendi inmea, pro ei omnis cetero copiosae. Pri et maiorum democritum disputando, augue nihil at ius. Te vis modo adipisci urbanitas, cudicam alienum mediocritatem<br/>    nec, mea an laoreet tincidunt. Cum ea recteque periculis assentior, veri liber cu pri, eum solumdisputando et. His invidunt assueverit ad, enim movet voluptatibus ei est, cu rebum incorrupte mei. Sit quas minim labore            id.",
             img: 'img/Frogger.png',
+            bigimg: 'img/Frogger.png',
+            github: 'https://github.com/tahahojati/FEWP3-Frogger',
+            live: 'https://tahahojati.github.io/FEWP3-Frogger/',
+            frameworks: ['angular', 'javascript'],
+            imgs: ['img/Frogger.png', 'img/Frogger.png'],
+            description: "Lorem ipsum dolor sit amet, paulo admodum an sit, denique noluisse vim ne. Vix cu vocent regione insolens. Sed te purto debitis molestiae. Ut quod dico alienum nam, odio iudico oportere eu vis. Sea aliquam maluisset hendrerit et, sea eu expetendis efficiendi<br/> nstructior, semper dictas mei ad. Aliquid appellantur qui ea, eum posse invidunt necessitatibus at, accusam apeirian per in. Vel et velit interpretaris. Sit ut porro melius habemus, consetetur dissentias vel cu. Iisque<br/>  integre lucilius pro no,an dico veritus est. Brute recusabo mea ei, quo sint constituam deterruisset te, velit.",
+            body:'Lorem ipsum dolor sit amet, paulo admodum an sit, denique noluisse vim ne. Vix cu vocent regione insolens. Sed te purto debitis molestiae. Ut quod dico alienum nam, odio iudico oportere eu vis. Sea aliquam maluisset hendrerit et, sea eu expetendis efficiendi<br/> nstructior, semper dictas mei ad. Aliquid appellantur qui ea, eum posse invidunt necessitatibus at, accusam apeirian per in. Vel et velit interpretaris. Sit ut porro melius habemus, consetetur dissentias vel cu. Iisque<br/>  integre lucilius pro no,an dico veritus est. Brute recusabo mea ei, quo sint constituam deterruisset te, velit ubique ei mea. Cum eu inani copiosae aliquando.Cum an vide graece utamur, his et dico equidem vulputate. Exercinoster ea vix. Ad amet dicunt vix, modus oblique in duo. Sonet docendi inmea, pro ei omnis cetero copiosae. Pri et maiorum democritum disputando, augue nihil at ius. Te vis modo adipisci urbanitas, cudicam alienum mediocritatem<br/>    nec, mea an laoreet tincidunt. Cum ea recteque periculis assentior, veri liber cu pri, eum solumdisputando et. His invidunt assueverit ad, enim movet voluptatibus ei est, cu rebum incorrupte mei. Sit quas minim labore',
 
         },
         {
@@ -69,11 +96,6 @@ angular.module('taha', ['ui.router'])
     ];
     return fac;
 }] )
-.controller('PortfolioController', ['$scope', 'projectFactory', function($scope ,projectFactory) {
-
-    $scope.projects = projectFactory;
-}])
-
 
 
 
