1d2ce1ba5e71ab8bed94ad792e7e1f289051d442
Taha Pourjalali <taha.hojati@gmail.com>
2016-06-17 20:10:30 -1000

    Added OPIHI

--- js/app.js
+++ js/app.js
@@ -85,7 +85,13 @@ angular.module('taha', ['ui.router','ngSanitize'])
             icon: 'img/cordiva.png'
         },
         'bootstrap':{
-            icon: 'bootstrap.png'
+            icon: 'img/bootstrap.png'
+        },
+        'css3':{
+            icon: 'img/css3.svg'
+        },
+        'html5':{
+            icon: 'img/html5.png'
         }
     };
 }])
@@ -96,13 +102,13 @@ angular.module('taha', ['ui.router','ngSanitize'])
             id: 0,
             title: 'OPIHI: Our Project in Hawaii\'s Intertidal ',
             subtitle:'Under active development',
-            img: 'img/Frogger-pre.png',
-            bigimg: 'img/Frogger.png 1041w, img/Frogger-lg.png 1540w',
-            github: 'https://github.com/tahahojati/FEWP3-Frogger',
-            live: 'https://tahahojati.github.io/FEWP3-Frogger/',
-            frameworks: ['angular', 'javascript'],
-            description: "Lorem  deterruisset te, velit.",
-            bodyRows:[{text:'Lorem ipsum ', imgs: 'hello'}],
+            img: 'img/OPIHI-pre.png',
+            bigimg: 'img/OPIHI.png 1041w, img/OPIHI-lg.png 1540w',
+            github: 'https://github.com/tahahojati/CRDG_OPIHI',
+            frameworks: ['zf', 'angular', 'bootstrap'],
+            description: "OPIHI is a school-based monitoring program of Hawaii’s rocky intertidal where students improve their scientific skills while providing assistance gathering data in an area that has not been well studied in Hawaii. <br/> <br/> While taking part in OPIHI, students will learn about topics in marine ecology and conservation, species identification, and sampling techniques. Most importantly, students will build their confidence and interest in science. OPIHI provides opportunities for students to develop and investigate their own questions within the framework of monitoring biodiversity in the intertidal. We hope this place-based learning connects students to their local environments and natural resources and encourages wise stewardship ...",
+            bodyRows:[{text:"<h2> Introduction </h2> <p> OPIHI is a school-based monitoring program of Hawaii’s rocky intertidal where students improve their scientific skills while providing assistance gathering data in an area that has not been well studied in Hawaii. <br/> <br/> While taking part in OPIHI, students will learn about topics in marine ecology and conservation, species identification, and sampling techniques. Most importantly, students will build their confidence and interest in science. OPIHI provides opportunities for students to develop and investigate their own questions within the framework of monitoring biodiversity in the intertidal. We hope this place-based learning connects students to their local environments and natural resources and encourages wise stewardship. <p> <p> I was hired by CRDG to design a website in order to facilitate data input for the students.  The website will also serve as a front-end for general public to view the studies's results and summary graphs. The website is being developed using Zend Framework for backend; while its front end takes advange of Bootstrap and AngularJS for displaying the content.</p>", imgs: '<img class="img-responsive" src="img/OPIHI3.png"> '},
+        {text:"<h2> Current State </h2> <p> This project is under active development.  Currently, the website has a complete user model but only a partial model for field studies.  The project's development was halted for more than a month, but I have recently restarted its develpment.  The expected finish date for this project is end of August. I am also interested in creating a hybrid mobile app for the project afterwards. At this time, I cannot provide a live preview for this project, but this will change as development progresses.</p>", imgs: '<img class="img-responsive  body-row" src="img/OPIHI1.png"> <img class="img-responsive" src="img/OPIHI2.png">'}],
 
         },
         {
@@ -113,7 +119,7 @@ angular.module('taha', ['ui.router','ngSanitize'])
             bigimg: 'img/Frogger.png 1041w, img/Frogger-lg.png 1540w',
             github: 'https://github.com/tahahojati/FEWP3-Frogger',
             live: 'https://tahahojati.github.io/FEWP3-Frogger/',
-            frameworks: ['angular', 'javascript'],
+            frameworks: ['css3', 'javascript'],
             description: "Lorem  deterruisset te, velit.",
             bodyRows:[{text:'Lorem ipsum ', imgs: 'hello'}],
 
