039b908e8ead92bb9768876f983f8538ad9dc9ab
Taha Pourjalali <taha.hojati@gmail.com>
2016-08-31 15:35:50 -1000

    took off the github for the aashto and opihi.  Added live preview for opihi. minor changes on other places.

--- js/app.js
+++ js/app.js
@@ -63,6 +63,15 @@ angular.module('taha', ['ui.router', 'ngSanitize'])
 
 .factory('frameworkProvider', [function() {
     return {
+        'node': {
+            icon: 'img/nodejs-logo.png'
+        },
+        'mean': {
+            icon: 'img/MEAN.png'
+        },
+        'd3': {
+            icon: 'img/d3.png'
+        },
         'angular': {
             icon: 'img/angular.png'
         },
@@ -105,14 +114,15 @@ angular.module('taha', ['ui.router', 'ngSanitize'])
         subtitle: 'Under active development',
         img: 'img/OPIHI-pre.png',
         bigimg: 'img/OPIHI.png 1041w, img/OPIHI-lg.png 1540w',
-        github: 'https://github.com/tahahojati/CRDG_OPIHI',
-        frameworks: ['zf', 'angular', 'bootstrap'],
+        // github: 'https://github.com/tahahojati/CRDG_OPIHI',
+        live:'http://programs.crdg.hawaii.edu:81',
+        frameworks: ['zf', 'jquery', 'bootstrap', 'd3'],
         description: "OPIHI is a school-based monitoring program of Hawaii’s rocky intertidal where students improve their scientific skills while providing assistance gathering data in an area that has not been well studied in Hawaii. <br/> <br/> While taking part in OPIHI, students will learn about topics in marine ecology and conservation, species identification, and sampling techniques. Most importantly, students will build their confidence and interest in science. OPIHI provides opportunities for students to develop and investigate their own questions within the framework of monitoring biodiversity in the intertidal. We hope this place-based learning connects students to their local environments and natural resources and encourages wise stewardship ...",
         bodyRows: [{
             text: "<h2> Introduction </h2> <p> OPIHI is a school-based monitoring program of Hawaii’s rocky intertidal where students improve their scientific skills while providing assistance gathering data in an area that has not been well studied in Hawaii. <br/> <br/> While taking part in OPIHI, students will learn about topics in marine ecology and conservation, species identification, and sampling techniques. Most importantly, students will build their confidence and interest in science. OPIHI provides opportunities for students to develop and investigate their own questions within the framework of monitoring biodiversity in the intertidal. We hope this place-based learning connects students to their local environments and natural resources and encourages wise stewardship. <p> <p> I was hired by CRDG to design a website in order to facilitate data input for the students.  The website will also serve as a front-end for general public to view the studies's results and summary graphs. The website is being developed using Zend Framework for backend; while its front end takes advange of Bootstrap and AngularJS for displaying the content.</p>",
             imgs: '<img class="img-responsive" src="img/OPIHI3.png"> '
         }, {
-            text: "<h2> Current State </h2> <p> This project is under active development.  Currently, the website has a complete user model but only a partial model for field studies.  The project's development was halted for more than a month, but I have recently restarted its develpment.  The expected finish date for this project is end of August. I am also interested in creating a hybrid mobile app for the project afterwards. At this time, I cannot provide a live preview for this project, but this will change as development progresses.</p>",
+            text: "<h2> Current State </h2> <p> This project is under active development.  Currently, the website is capable of gathering fieldtrip data.  The expected deployment date for this project is end of October. You may visit the live preview of the project by clicking on the link provided in the cover picture above.  You may login to the preview website as administrator using email t@t.com and password tttttt.  Note that several features of the website do not function on the preview due to the limitations of our in house server.",
             imgs: '<img class="img-responsive  body-row" src="img/OPIHI1.png"> <img class="img-responsive" src="img/OPIHI2.png">'
         }],
 
@@ -121,12 +131,12 @@ angular.module('taha', ['ui.router', 'ngSanitize'])
         subtitle: 'Under active development',
         img: 'img/aashto-pre.png',
         bigimg: 'img/aashto.png 1041w, img/aashto-lg.png 1540w',
-        github: 'https://github.com/tahahojati/AASHTO',
+        // github: 'https://github.com/tahahojati/AASHTO',
         live: 'http://aashtobridgedesign.com/',
-        frameworks: ['zf', 'angular', 'bootstrap'],
-        description: "This website is being designed to assist structural engineers in their Aashto LFRD bridge design through providing important calculations and saving them on cloud for later access.  The project is still in its infancy as I only started it one week ago.  The expected release date for the project is end of August.",
+        frameworks: ['mean', 'node', 'bootstrap', 'angular'],
+        description: "This website is being designed to assist structural engineers in their Aashto LFRD bridge design through providing important calculations and saving them on cloud for later access. ",
         bodyRows: [{
-            text: "<h2>AASHTO LFRD Bridge Design </h2> <p>This website is being designed to assist structural engineers in their Aashto LFRD bridge design through providing important calculations and saving them on cloud for later access.  The project is still in its infancy as I only started it one week ago.  The expected release date for the project is end of August.</p>",
+            text: "<h2>AASHTO LFRD Bridge Design </h2> <p>This website is being designed to assist structural engineers in their Aashto LFRD bridge design through providing important calculations and saving them on cloud for later access.  </p>",
             imgs: '<div class="col-xs-12 col-sm-4  body-row" > <img class="img-responsive" src="img/aashto1.png" > </div> <div class="col-xs-12 col-sm-4  body-row" > <img class="img-responsive" src="img/aashto2.png" > </div> <div class="col-xs-12 col-sm-4  body-row" > <img class="img-responsive" src="img/aashto3.png" > </div>',
             layout: 2
         }],
@@ -161,7 +171,7 @@ angular.module('taha', ['ui.router', 'ngSanitize'])
         img: 'img/coffee-pre.png',
         bigimg: 'img/coffee.png 1041w, img/coffee-lg.png 1540w',
         github: 'https://github.com/tahahojati/FEWP5A-Honolulu-Coffee',
-        live: 'https://tahahojati.github.io/FEWP3-Frogger/',
+        live: 'https://tahahojati.github.io/FEWP5A-Honolulu-Coffee/',
         docs: 'https://tahahojati.github.io/FEWP5A-Honolulu-Coffee/out/index.html',
         frameworks: ['knockout', 'javascript', 'maps', 'jquery'],
         description: "This project lists some of the best coffeeshops in Honolulu. The location of the shops is shown on an embedded map. The FourSquare rating for each location is also provided. This project takes advantage of KnockoutJS and jQuery's AJAX functions to get data from FourSquare.com via asynchronous requests.",
