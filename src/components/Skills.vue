c53c7b51bfc29be26406c036a755286f6e89126f
Taha <taha.hojati@gmail.com>
2018-03-18 09:32:33 -1000

    added and modified tagcnvas

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,18 +5,13 @@
 	<div class="text-zone">
 		<h1> Skills &#038; Experience
 		</h1>
-		<p>Main area of my expertise is front end development and everything related with this side of web.   HTML,CSS, JS (ES5,ES6), building small &amp; medium Web Apps, Angular , Vue.js, building custom plugins, features, animations and just coding layouts.
+		<p>I am mostly 
 		</p>
-		<p>Often I work as a full-stack develeoper  &#8211; mainly in Lamp environment, PHP and open source CMS&#8217;s like (WordPress, Drupal, Magento, Typo 3 etc..) .
+		<p>
 		</p>
-		<p>I have also some experience with Node.js,  Mongo DB (Keystone.JS).
+		<p>
 		</p>
-		<p>Would like to know more?
-			<br /> Please check my
-			<a href="https://www.linkedin.com/in/jacek-jeznach-0a99b640" target="_blank">LinkedIn
-			</a> profile or just
-			<a href="/contact">contact
-			</a> me.
+		<p>
 		</p>
 	</div>
 	<div class="clear">
@@ -26,7 +21,7 @@
 			<canvas width="500" height="500" id="myCanvas">
 			<!-- TODO: add the sphere of skills -->
 			</canvas>
-		</div>
+		</div>w
 	</div>
 	<span class="tags bottom-tags"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;
 		<br/> &lt;/html&gt;
