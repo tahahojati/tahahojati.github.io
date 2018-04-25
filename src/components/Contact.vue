00a7adfed120aa78255c6f3bd5479711cd32a74a
Taha Pourjalali <taha.hojati@gmail.com>
2018-04-25 10:03:51 -1000

    Contact form shows and has an appear transition

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,36 +7,38 @@
 		</h1>
 		<p>If you have any questions, please don&#8217;t hesitate to contact using form below&#8230;
 		</p>
-		<div class="contact-form">
-			<form id="contact" autocomplete="off">
-				<ul>
-					<li class="half">
-						<input class="input__field input__field--hoshi" placeholder="Name" type="text" name="name" />
-						<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
-						</label>
-					</li>
-					<li class="half">
-						<input class="input__field input__field--hoshi" placeholder="Email" type="email" name="email" />
-						<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
-						</label>
-					</li>
-					<li>
-						<input class="input__field input__field--hoshi" placeholder="Subject" type="text" name="subject" />
-						<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
-						</label>
-					</li>
-					<li>
-						<textarea class="input__field input__field--hoshi" placeholder="Message" name="msg">
-						</textarea>
-						<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
-						</label>
-					</li>
-					<li class="submit">
-						<input id="submit" type="submit" class="flat-button" value="SEND"/>
-					</li>
-				</ul>
-			</form>
-		</div>
+		<transition name="fade" appear>
+			<div class="contact-form">
+				<form id="contact" autocomplete="off">
+					<ul>
+						<li class="half">
+							<input class="input__field input__field--hoshi" placeholder="Name" type="text" name="name" />
+							<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
+							</label>
+						</li>
+						<li class="half">
+							<input class="input__field input__field--hoshi" placeholder="Email" type="email" name="email" />
+							<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
+							</label>
+						</li>
+						<li>
+							<input class="input__field input__field--hoshi" placeholder="Subject" type="text" name="subject" />
+							<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
+							</label>
+						</li>
+						<li>
+							<textarea class="input__field input__field--hoshi" placeholder="Message" name="msg">
+							</textarea>
+							<label class="input__label input__label--hoshi input__label--hoshi-color-2" >
+							</label>
+						</li>
+						<li class="submit">
+							<input id="submit" type="submit" class="flat-button" value="SEND"/>
+						</li>
+					</ul>
+				</form>
+			</div>
+		</transition>
 	</div>
 	<div class="inf-map"> Jacek Jeznach,
 		<br/> Poland,ul. Bartosza Głowackiego 28 lok. 22, 20-060 Lublin
@@ -56,3 +58,15 @@
 	</span>
 </div>
 </template>
+
+<style>
+.contact-form{
+	opacity: 0.9; 
+}
+.fade-enter{
+	opacity:0;
+}
+.fade-enter-active{
+	transition: opacity 3s ease;
+}
+</style>
